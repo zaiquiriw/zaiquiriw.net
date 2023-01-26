2759bb4f6d0db1bd01762c4aa433484d12361878
Zachary Canoot <zaiquiriw@gmail.com>
2023-01-26 12:28:49 -0600

    Set up next.js

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,30 +1,17 @@
-import Link from "next/link";
-import Head from "next/head";
-// import Script from "next/script"; Script example
-import Layout from "../../components/layout";
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../../components/layout'
 
 export default function FirstPost() {
-	return (
-		<Layout>
-			<Head>
-				<title>First Post</title>
-			</Head>
-
-			{/* I forgot to brace the multiline comment here for so long!
-				Example of a Script Component
-			<Script
-				src="https://connect.facebook.net/en_US/sdk.js"
-				strategy="lazyOnload"
-				onLoad={() =>
-					console.log(`script loaded correctly, window.FB has been populated`)
-				}
-			/>
-			 */}
-
-			<h1>First Post</h1>
-			<h2>
-				<Link href="/">Back to home</Link>
-			</h2>
-		</Layout>
-	);
+  return (
+    <Layout>
+      <Head>
+        <title>First Post</title>
+      </Head>
+      <h1>First Post</h1>
+      <h2>
+        <Link href="/">Back to home</Link>
+      </h2>
+    </Layout>
+  )
 }
